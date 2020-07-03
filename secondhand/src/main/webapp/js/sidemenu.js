630f0321ac903c58d91f63ce1269d060bb36073e
deokkk <kims18@nate.com>
2020-07-03 15:18:10 +0900

    [김경덕 남상현] sidemenu.html, css, js파일 추가 noticeList.html에 sidemenu 적용 searchWord.css #fixed-bar #fixed-bar-wrap margin속성 제거

---
+++ secondhand/src/main/webapp/js/sidemenu.js
@@ -0,0 +1,8 @@
+// sidemenu
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
